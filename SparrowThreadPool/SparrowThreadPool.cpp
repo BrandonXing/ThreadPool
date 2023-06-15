718d3ad286c7a5f0902fb928f72ac9dd66698b8d
BrandonXing <wotingfengyu@live.cn>
2023-06-15 16:25:43 +0800

    Add new implement

---
+++ SparrowThreadPool/SparrowThreadPool.cpp
@@ -0,0 +1,25 @@
+﻿// SparrowThreadPool.cpp: 定义应用程序的入口点。
+//
+#include <chrono>
+#include "SparrowThreadPool.h"
+
+using namespace std;
+
+int main()
+{
+	TaskPool threadpool;
+	threadpool.init();
+
+	Task* task = nullptr;
+	for(int i=0;i<10;++i)
+	{
+		task = new Task();
+		threadpool.addTask(task);
+	}
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+
+	threadpool.stop();
+
+	return 0;
+}
